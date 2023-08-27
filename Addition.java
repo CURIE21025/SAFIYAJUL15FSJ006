83684794ccee94fb2c3d057e3f8ee546bfd33aa1
Safiya Parveen A R <86617526+CURIE21025@users.noreply.github.com>
2023-08-27 20:48:08 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Addition {
+ public static void main(String[] args) {
+	Scanner s =new Scanner(System.in);
+	System.out.println("Enter a number:");
+	int x=s.nextInt();
+	System.out.println("Enter a number:");
+ int y=s.nextInt();
+		 System.out.println("Sum of the two numbers "+x+","+y+ " is "+(x+y));
+		 
+		 
+		 
+ }
+ }
